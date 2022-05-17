5003bb752a8fd57222e3fb3a0257df8089339ff9
Emre020 <94354231+Emre020@users.noreply.github.com>
2022-05-17 08:51:30 +0200

    FirstPersonContoller

---
+++ Prototyping2/Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    //Geschwindigkeit des Spielers
+    public float speed = 12;
+
+    //Gravitation des Spielers
+    public float gravity = -9.81f;
+
+    //Springen
+    //public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask; //Für Layer
+
+    //Gravitation
+    Vector3 velocity;
+
+    //Boden prüfen
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Überprüfe ob der Spieler auf den Boden ist
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        //Steuerung nach Links und Rechts
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        //Springen
+        /*if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }*/
+
+        //Gravitation
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
